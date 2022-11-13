d663f6012fece165b4d9183a4a01002ca539e911
leek-emperor <1332590314@qq.com>
2022-11-13 10:47:14 +0800

    1.10

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: liutq
  * @Date: 2022-10-26 23:02:28
- * @LastEditTime: 2022-11-12 16:38:53
+ * @LastEditTime: 2022-11-13 10:44:37
  * @LastEditors: liutq
  * @Reference:
  */
@@ -13,35 +13,50 @@ Login
 Editor
 Myself
 */
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { unstable_HistoryRouter as HistoryRouer, Route, Routes } from 'react-router-dom';
 import { history } from '../utils/history';
-import Login from '../pages/Login';
-import Detail from '../pages/Detail';
-import Article from '../pages/Article';
-import Edit from '../pages/Edit';
-import Setting from '../pages/Settings';
-import PersonalSetting from '../pages/PersonalSetting';
-import AccountSetting from '../pages/AccountSetting';
-import App from '../App';
 import { observer } from 'mobx-react-lite';
-import Home from '../pages/Home';
+
+const Home = lazy(() => import('../pages/Home'));
+const Login = lazy(() => import('../pages/Login'));
+const Detail = lazy(() => import('../pages/Detail'));
+const Article = lazy(() => import('../pages/Article'));
+const Edit = lazy(() => import('../pages/Edit'));
+const Setting = lazy(() => import('../pages/Settings'));
+const PersonalSetting = lazy(() => import('../pages/PersonalSetting'));
+const AccountSetting = lazy(() => import('../pages/AccountSetting'));
+const App = lazy(() => import('../App'));
+
 function index() {
 	return (
 		<HistoryRouer history={history}>
-			<Routes>
-				<Route path="/" element={<App />}>
-					<Route index element={<Article />}></Route>
-					<Route path="detail" element={<Detail />}></Route>
-					<Route path="home" element={<Home />}></Route>
-					<Route path="setting" element={<Setting />}>
-						<Route index element={<PersonalSetting />}></Route>
-						<Route path="account" element={<AccountSetting />}></Route>
+			<Suspense
+				fallback={
+					<div
+						style={{
+							textAlign: 'center',
+							marginTop: 200,
+						}}
+					>
+						loading...
+					</div>
+				}
+			>
+				<Routes>
+					<Route path="/" element={<App />}>
+						<Route index element={<Article />}></Route>
+						<Route path="detail" element={<Detail />}></Route>
+						<Route path="home" element={<Home />}></Route>
+						<Route path="setting" element={<Setting />}>
+							<Route index element={<PersonalSetting />}></Route>
+							<Route path="account" element={<AccountSetting />}></Route>
+						</Route>
 					</Route>
-				</Route>
-				<Route path="/login" element={<Login />}></Route>
-				<Route path="/edit" element={<Edit />}></Route>
-			</Routes>
+					<Route path="/login" element={<Login />}></Route>
+					<Route path="/edit" element={<Edit />}></Route>
+				</Routes>
+			</Suspense>
 		</HistoryRouer>
 	);
 }
